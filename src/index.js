4377a5810dfc7fb38059e2363630c8655dd9a1ff
joaohenriquefernandes <joaohenriquefernandes.dev@gmail.com>
2025-01-07 11:20:02 -0300

    chore: configure eslint and editorConfig

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-const express = require('express')
+const express = require('express');
 
-const app = express()
+const app = express();
 
-app.listen(3000, () => console.log('Server started at http://localhost:3000'))
+app.listen(3000, () => console.log('Server started at http://localhost:3000'));
