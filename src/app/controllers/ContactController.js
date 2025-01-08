42c959dedc6c6bb55f251b772ce0c17eceeb6cab
joaohenriquefernandes <joaohenriquefernandes.dev@gmail.com>
2025-01-08 16:17:38 -0300

    feat: create contact controller

---
+++ src/app/controllers/ContactController.js
@@ -0,0 +1,23 @@
+class ContactController {
+  index(request, response) {
+    response.send('Hello')
+  }
+
+  show() {
+
+  }
+
+  store() {
+
+  }
+
+  update() {
+
+  }
+
+  delete() {
+
+  }
+}
+
+module.exports = new ContactController()
