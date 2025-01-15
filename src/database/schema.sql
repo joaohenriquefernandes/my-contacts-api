9a8ffd34efd44c8e2f5f6723e3adfc9da1a37970
joaohenriquefernandes <joaohenriquefernandes.dev@gmail.com>
2025-01-15 17:54:37 -0300

    db: create database schema

---
+++ src/database/schema.sql
@@ -0,0 +1,21 @@
+DROP DATABASE IF EXISTS my_contacts;
+
+CREATE DATABASE my_contacts;
+
+\c my_contacts
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS categories (
+  id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
+  name VARCHAR NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS contacts (
+  id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
+  name VARCHAR NOT NULL,
+  email VARCHAR UNIQUE,
+  phone VARCHAR,
+  category_id UUID,
+  FOREIGN KEY(category_id) REFERENCES categories(id)
+);
